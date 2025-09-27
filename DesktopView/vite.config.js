53af8495799d3653b5bb7ae4e116be378ca50b1d
bayo <yekeenadebayo01@gmail.com>
2025-09-27 17:23:15 +0100

    new updateS

--- DesktopView/vite.config.js
+++ DesktopView/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace 'your-repo-name' with your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
+  base: '/KORNA/'
 })
